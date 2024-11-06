3bcbb020250cab4c5f102b0853e2309aee03235c
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2024-11-06 17:13:01 -0300

    Colorindo o Menu.

--- src/conta/util/Cores.java
+++ src/conta/util/Cores.java
@@ -1,5 +1,19 @@
 package conta.util;
 
 public class Cores {
-
+			
+			//Resetar as Cores
+			public static final String TEXT_RESET = "\u001B[0m";
+			
+			//Cores do Texto
+			public static final String TEXT_BLACK = "\u001B[30m";
+			public static final String TEXT_BLUE = "\u001B[34m";
+			public static final String TEXT_CYAN = "\u001B[36m";
+			public static final String TEXT_WHITE = "\u001B[37m";
+			
+			// Cores de Fundo.
+			public static final String BLACK_BACKGROUND = "\u001B[40m";
+			public static final String CYAN_BACKGROUND = "\u001B[46m";
+			public static final String WHITE_BACKGROUND = "\u001B[47m";
+					
 }
