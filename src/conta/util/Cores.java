bc286acfb774d510c85c5c144a65189736f5df19
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2024-11-06 17:16:30 -0300

    Implementando todos os Atributos da classe Cores

--- src/conta/util/Cores.java
+++ src/conta/util/Cores.java
@@ -1,19 +1,77 @@
 package conta.util;
 
 public class Cores {
+	//Resetar Cores
+	public static final String TEXT_RESET = "\u001B[0m";
+
+	//Cor do Texto
+	public static final String TEXT_BLACK = "\u001B[30m";
+	public static final String TEXT_RED = "\u001B[31m";
+	public static final String TEXT_GREEN = "\u001B[32m";
+	public static final String TEXT_YELLOW = "\u001B[33m";
+	public static final String TEXT_BLUE = "\u001B[34m";
+	public static final String TEXT_PURPLE = "\u001B[35m";
+	public static final String TEXT_CYAN = "\u001B[36m";
+	public static final String TEXT_WHITE = "\u001B[37m";
+
+	//Cor do Texto em Negrito
+	public static final String TEXT_BLACK_BOLD = "\033[1;30m";
+	public static final String TEXT_RED_BOLD = "\033[1;31m";
+	public static final String TEXT_GREEN_BOLD = "\033[1;32m";
+	public static final String TEXT_YELLOW_BOLD = "\033[1;33m"; 
+	public static final String TEXT_BLUE_BOLD = "\033[1;34m";
+	public static final String TEXT_PURPLE_BOLD = "\033[1;35m";
+	public static final String TEXT_CYAN_BOLD = "\033[1;36m";
+	public static final String TEXT_WHITE_BOLD = "\033[1;37m";
+
+	//Resetar texto sobrelinha.
+	public static final String TEXT_BLACK_UNDERLINED = "\033[4;30m"; 
+	public static final String TEXT_RED_UNDERLINED = "\033[4;31m"; 
+	public static final String TEXT_GREEN_UNDERLINED = "\033[4;32m"; 
+	public static final String TEXT_YELLOW_UNDERLINED = "\033[4;33m"; 
+	public static final String TEXT_BLUE_UNDERLINED = "\033[4;34m"; 
+	public static final String TEXT_PURPLE_UNDERLINED = "\033[4;35m"; 
+	public static final String TEXT_CYAN_UNDERLINED = "\033[4;36m"; 
+	public static final String TEXT_WHITE_UNDERLINED = "\033[4;37m"; 
 			
-			//Resetar as Cores
-			public static final String TEXT_RESET = "\u001B[0m";
+	// Cor do texto de Alta Intensidade(brilho)
+	public static final String TEXT_BLACK_BRIGHT = "\033[0;90m";  
+	public static final String TEXT_RED_BRIGHT = "\033[0;91m";    
+	public static final String TEXT_GREEN_BRIGHT = "\033[0;92m";  
+	public static final String TEXT_YELLOW_BRIGHT = "\033[0;93m"; 
+	public static final String TEXT_BLUE_BRIGHT = "\033[0;94m";  
+	public static final String TEXT_PURPLE_BRIGHT = "\033[0;95m"; 
+	public static final String TEXT_CYAN_BRIGHT = "\033[0;96m";   
+	public static final String TEXT_WHITE_BRIGHT = "\033[0;97m";  
+
+	// Cor do texto de alta intensidade em Negrito
+	public static final String TEXT_BLACK_BOLD_BRIGHT = "\033[1;90m"; 
+	public static final String TEXT_RED_BOLD_BRIGHT = "\033[1;91m";   
+	public static final String TEXT_GREEN_BOLD_BRIGHT = "\033[1;92m"; 
+	public static final String TEXT_YELLOW_BOLD_BRIGHT = "\033[1;93m";
+	public static final String TEXT_BLUE_BOLD_BRIGHT = "\033[1;94m";  
+	public static final String TEXT_PURPLE_BOLD_BRIGHT = "\033[1;95m";
+	public static final String TEXT_CYAN_BOLD_BRIGHT = "\033[1;96m";  
+	public static final String TEXT_WHITE_BOLD_BRIGHT = "\033[1;97m"; 
+
+	// Cores de Fundo
+	public static final String BLACK_BACKGROUND = "\u001B[40m";
+	public static final String RED_BACKGROUND = "\u001B[41m";
+	public static final String GREEN_BACKGROUND = "\u001B[42m";
+	public static final String YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String BLUE_BACKGROUND = "\u001B[44m";
+	public static final String PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String CYAN_BACKGROUND = "\u001B[46m";
+	public static final String WHITE_BACKGROUND = "\u001B[47m";
 			
-			//Cores do Texto
-			public static final String TEXT_BLACK = "\u001B[30m";
-			public static final String TEXT_BLUE = "\u001B[34m";
-			public static final String TEXT_CYAN = "\u001B[36m";
-			public static final String TEXT_WHITE = "\u001B[37m";
-			
-			// Cores de Fundo.
-			public static final String BLACK_BACKGROUND = "\u001B[40m";
-			public static final String CYAN_BACKGROUND = "\u001B[46m";
-			public static final String WHITE_BACKGROUND = "\u001B[47m";
+	// Cores de fundo de Alta Intensidade
+	public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";
+	public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";
+	public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";
+	public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";
+	public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";
+	public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; 
+	public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  
+	public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";
 					
 }
